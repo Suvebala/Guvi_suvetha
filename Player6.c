406c795a89bdfb340fa9fbff0f95f9804ba3b486
Suvetha <49278794+Suvebala@users.noreply.github.com>
2019-04-04 19:02:03 +0530

    Update Player6.c

--- Player6.c
+++ Player6.c
@@ -2,21 +2,21 @@
 #include<string.h>
 int main(void) 
 {
-	char a[350],b[350];
-	scanf("%s %s",a,b);
+	char p[350],q[350];
+	scanf("%s %s",p,q);
 	int m,n,i,j,u,k,l,x,y,z,flag=0;
-	m=strlen(a);
-	n=strlen(b);
+	m=strlen(p);
+	n=strlen(q);
 	if(m==n)
 	{
 	for(i=0;i<m;i++)
 	{
 	for(j=i+1;j<m;j++)
 	{
-	u=a[i];
-	k=a[j];
-	l=b[i];
-	x=b[j];
+	u=p[i];
+	k=p[j];
+	l=q[i];
+	x=q[j];
   y=u-k;
 	z=l-x;
 	if(y==z)
